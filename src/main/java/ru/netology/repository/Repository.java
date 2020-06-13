58ad8c544ffe09417762b9cb72856c37d800f233
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-06-13 10:13:18 +0300

    add Repository

---
+++ src/main/java/ru/netology/repository/Repository.java
@@ -0,0 +1,50 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Movie;
+
+public class Repository {
+    private Movie[] movies = new Movie[0];
+
+    public Movie[] findAll() {
+        return movies;
+    }
+
+    public void save(Movie movie) {
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public Movie[] findById(int id) {
+        Movie[] tmp = new Movie[0];
+        int index = 0;
+        for (Movie movie : movies) {
+            if (movie.getId() == id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        return tmp;
+    }
+
+    public void removeById(int id) {
+        int length = movies.length - 1;
+        Movie[] tmp = new Movie[length];
+        int index = 0;
+        for (Movie movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        movies = tmp;
+    }
+
+    public Movie[] removeAll() {
+        return movies = new Movie[0];
+    }
+
+}
